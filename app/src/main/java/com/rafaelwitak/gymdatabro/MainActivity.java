39221ffe6cf6a16727862e7c276f8eb7759c1b1b
RWitak <rafael.witak@gmail.com>
2020-08-05 13:03:08 +0200

    add database class, WorkoutStepActivity, database build.

--- app/src/main/java/com/rafaelwitak/gymdatabro/MainActivity.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/MainActivity.java
@@ -2,15 +2,24 @@ package com.rafaelwitak.gymdatabro;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.room.Room;
 
 import android.os.Bundle;
 
+import com.rafaelwitak.gymdatabro.database.GymDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    public GymDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = Room
+                .databaseBuilder(getApplicationContext(), GymDatabase.class, "gym_data")
+                .allowMainThreadQueries()
+                .build();
 
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
     }
