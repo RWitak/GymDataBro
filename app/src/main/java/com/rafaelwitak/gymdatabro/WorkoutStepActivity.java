4490bd0352f73ec9727941f3cbc01efd94e04a44
RWitak <rafael.witak@gmail.com>
2020-08-09 18:33:11 +0200

    create WorkoutStepActivity incl. XML layout, add some Views

---
+++ app/src/main/java/com/rafaelwitak/gymdatabro/WorkoutStepActivity.java
@@ -0,0 +1,14 @@
+package com.rafaelwitak.gymdatabro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkoutStepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_step);
+    }
+}
