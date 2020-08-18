fb6cce36da36dcff04051914bee24159d9d7fd24
RWitak <rafael.witak@gmail.com>
2020-08-18 20:20:01 +0200

    first steps (incl. binding Views) in WorkoutStepActivity
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/WorkoutStepActivity.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/WorkoutStepActivity.java
@@ -1,14 +1,45 @@
 package com.rafaelwitak.gymdatabro;
 
+import androidx.annotation.LayoutRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.room.Database;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.rafaelwitak.gymdatabro.database.GymBroDatabase;
+import com.rafaelwitak.gymdatabro.database.WorkoutStep;
+import com.rafaelwitak.gymdatabro.database.WorkoutStepDAO;
+import com.rafaelwitak.gymdatabro.databinding.ActivityWorkoutStepBinding;
+
+import java.util.List;
 
 public class WorkoutStepActivity extends AppCompatActivity {
+
+    private ActivityWorkoutStepBinding binding;
+    private WorkoutStep currentWorkoutStep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_step);
+        database = ;
+        currentWorkoutStep = getCurrentWorkoutStep();
+
+        // bind all Views with IDs automatically
+        binding = ActivityWorkoutStepBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    private WorkoutStep getCurrentWorkoutStep() {
+        // TODO
+        int stepNumber = getIntent().getIntExtra("nextStepNumber", 0);
+        int workoutID = getIntent().getIntExtra("workoutID", 0);
+        LiveData<WorkoutStep> step
+        return step;
     }
 }
