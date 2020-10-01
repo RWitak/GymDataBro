cfedda5bb0b2a319da096f21a895bc5790d80beb
RWitak <rafael.witak@gmail.com>
2020-10-01 22:39:28 +0200

    Display Toolbar and "plus" Button in ChooseProgramActivity
    * Toolbar gets set up with title
    * Button starts MainActivity - for now
    * Some refactoring in ChooseProgramActivity
    
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/activities/ChooseProgramActivity.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/activities/ChooseProgramActivity.java
@@ -1,10 +1,15 @@
 package com.rafaelwitak.gymdatabro.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.rafaelwitak.gymdatabro.R;
 import com.rafaelwitak.gymdatabro.database.GymBroDatabase;
 import com.rafaelwitak.gymdatabro.database.Program;
@@ -13,15 +18,40 @@ import com.rafaelwitak.gymdatabro.viewRows.ChooseProgramRow;
 public class ChooseProgramActivity extends AppCompatActivity {
     GymBroDatabase database;
     LinearLayout programList;
+    Toolbar chooseProgramToolbar;
+    FloatingActionButton createProgramFAB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         database = MainActivity.database;
         setContentView(R.layout.activity_choose_program);
+        chooseProgramToolbar = findViewById(R.id.choose_program_toolbar);
+        createProgramFAB = findViewById(R.id.choose_program_button_create);
         programList = findViewById(R.id.program_list);
 
+        setupViews();
+    }
+
+    private void setupViews() {
+        setupToolbar();
         setupRows();
+        setupFAB(this);
+    }
+
+    private void setupFAB(Context context) {
+        createProgramFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setupToolbar() {
+        chooseProgramToolbar.setTitle(R.string.choose_program_toolbar_title);
     }
 
     private void setupRows() {
