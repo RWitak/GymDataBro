4feee29124ccc96c6dca3560ff5541ec0bb3937b
RWitak <rafael.witak@gmail.com>
2020-10-01 23:43:55 +0200

    Add table-based layout for CreateProgramActivity
    String resources extracted. Nothing hooked up yet programmatically.
    
    Signed-off-by: RWitak <rafael.witak@gmail.com>

---
+++ app/src/main/java/com/rafaelwitak/gymdatabro/activities/CreateProgramActivity.java
@@ -0,0 +1,16 @@
+package com.rafaelwitak.gymdatabro.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rafaelwitak.gymdatabro.R;
+
+public class CreateProgramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_program);
+    }
+}
