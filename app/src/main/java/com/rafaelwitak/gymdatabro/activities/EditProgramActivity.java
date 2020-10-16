59074364cdfd7ecf2464783fdaaba9076d5987c5
RWitak <rafael.witak@gmail.com>
2020-10-16 14:24:26 +0200

    Setup Toolbar, add Bool programNew in EditProgramActivity
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/activities/EditProgramActivity.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/activities/EditProgramActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.rafaelwitak.gymdatabro.database.GymBroDatabase;
 import com.rafaelwitak.gymdatabro.database.Program;
@@ -13,10 +14,12 @@ import com.rafaelwitak.gymdatabro.viewRows.EditProgramRowHolder;
 public class EditProgramActivity extends AppCompatActivity {
 
     private ActivityEditProgramBinding binding;
+    private boolean programNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        programNew = true;
 
         binding = ActivityEditProgramBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -25,7 +28,14 @@ public class EditProgramActivity extends AppCompatActivity {
     }
 
     private void setupViews() {
+        setupToolbar();
         setupEditButton();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = binding.editProgramToolbar.getRoot();
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(programNew ? "Create Program" : "Edit Program");
     }
 
     private void setupEditButton() {
