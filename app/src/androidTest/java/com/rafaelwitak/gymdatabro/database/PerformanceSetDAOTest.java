1030ebe3e5d23d35b162f9297ed22a854e27b627
RWitak <rafael.witak@gmail.com>
2020-10-29 16:52:44 +0100

    [WIP] Test PerformanceSetDAO's id capabilities, extract utility methods for tests
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/androidTest/java/com/rafaelwitak/gymdatabro/database/PerformanceSetDAOTest.java
+++ app/src/androidTest/java/com/rafaelwitak/gymdatabro/database/PerformanceSetDAOTest.java
@@ -2,9 +2,60 @@ package com.rafaelwitak.gymdatabro.database;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+
 public class PerformanceSetDAOTest extends DaoTest {
 
     private PerformanceSetDAO dao;
+
+    @Test
+    public void constructorShouldWorkWithPartialInput() {
+        PerformanceSet psEmpty = new PerformanceSet();
+        PerformanceSet psNullEverywhere = new PerformanceSet(
+                null,
+                "TIMESTAMP",
+                123,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+                );
+        PerformanceSet psAllValues = new PerformanceSet(
+                23,
+                "2020-08-13",
+                123,
+                10,
+                22.5f,
+                12,
+                30,
+                8.5f,
+                0,
+                "Notes");
+
+        createExerciseWithId123();
+
+        assertThat(dao.insertSet(psAllValues)).isNotNull();
+        assertThat(dao.insertSet(psNullEverywhere)).isNotNull();
+        assertThat(dao.insertSet(psEmpty)).isNotNull();
+    }
+
+    private void createExerciseWithId123() {
+        database.exerciseDAO().insertNewExercise(
+                new Exercise(
+                        123,
+                        88.6f,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                        ));
+    }
 
     @Test
     public void getAllSets() {
@@ -28,7 +79,42 @@ public class PerformanceSetDAOTest extends DaoTest {
 
     @Test
     public void insertSet() {
-        //TODO: implement.
+    //TODO: implement.
+    }
+
+    @Test
+    public void nullIdGetsAutoFilled() {
+        createExerciseWithId123();
+        insertSetWithNullId();
+
+        List<PerformanceSet> performanceSetList = dao.getAllSets();
+
+        assertThat(performanceSetList.get(0).id).isNotNull();
+    }
+
+    @Test
+    public void nullIdGetsAutoIncremented() {
+        createExerciseWithId123();
+        insertSetWithNullId();
+        insertSetWithNullId();
+
+        List<PerformanceSet> performanceSetList = dao.getAllSets();
+
+        assertThat(performanceSetList.get(1).id).isNotEqualTo(performanceSetList.get(0).id);
+    }
+
+    private void insertSetWithNullId() {
+        dao.insertSet(new PerformanceSet(
+                null,
+                null,
+                123,
+                12,
+                30f,
+                60,
+                120,
+                8f,
+                0,
+                null));
     }
 
     @Test
