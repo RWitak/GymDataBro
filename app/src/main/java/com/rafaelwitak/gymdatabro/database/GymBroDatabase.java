be5388cf97f314183e31d7ab4be75704379ba872
RWitak <rafael.witak@gmail.com>
2020-11-05 22:25:54 +0100

    Remove destructiveMigration behaviour from DB
    Otherwise, the DB will be recreated at every app start, deleting previous entries.
    
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/database/GymBroDatabase.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/database/GymBroDatabase.java
@@ -49,7 +49,6 @@ public abstract class GymBroDatabase extends RoomDatabase {
                                     "gym_data")
                             .createFromAsset("gymdata.db")
                             .allowMainThreadQueries()
-                            .fallbackToDestructiveMigration()
                             .build();
                 }
             }
