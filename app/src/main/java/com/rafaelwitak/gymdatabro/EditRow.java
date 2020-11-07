59ac88716ceec6eebd2e6c48b831db6ddb1a9b89
RWitak <rafael.witak@gmail.com>
2020-11-07 11:13:41 +0100

    Extract superclass EditRow from EditProgramRow, pull some elements up
    Signed-off-by: RWitak <rafael.witak@gmail.com>

---
+++ app/src/main/java/com/rafaelwitak/gymdatabro/EditRow.java
@@ -0,0 +1,33 @@
+package com.rafaelwitak.gymdatabro;
+
+import android.widget.EditText;
+
+import com.rafaelwitak.gymdatabro.database.Program;
+
+public abstract class EditRow {
+    protected EditText editText;
+
+    public EditRow() {
+        this.editText = getEditText();
+    }
+
+    public Object getEditTextValue() {
+        return editText.getText();
+    }
+
+    public String getEditTextValueAsString() {
+        if (getEditTextValue().toString().isEmpty()) {
+            return "";
+        }
+        return getEditTextValue().toString().trim();
+    }
+
+    protected abstract EditText getEditText();
+
+    public void showErrorText(CharSequence error) {
+        this.editText.setError(error);
+        this.editText.requestFocus();
+    }
+
+    public abstract void setPreFilledText(Program program);
+}
