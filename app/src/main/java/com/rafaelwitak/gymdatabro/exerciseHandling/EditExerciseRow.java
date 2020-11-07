758687f86cf5172370bafcc0add53b10b578abba
RWitak <rafael.witak@gmail.com>
2020-11-07 11:31:30 +0100

    Compact EditExerciseRow, change binding private->protected
    Signed-off-by: RWitak <rafael.witak@gmail.com>

---
+++ app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/EditExerciseRow.java
@@ -0,0 +1,14 @@
+package com.rafaelwitak.gymdatabro.exerciseHandling;
+
+import com.rafaelwitak.gymdatabro.EditRow;
+import com.rafaelwitak.gymdatabro.databinding.ActivityEditExerciseBinding;
+
+public abstract class EditExerciseRow extends EditRow {
+
+    protected ActivityEditExerciseBinding binding;
+
+    public EditExerciseRow(ActivityEditExerciseBinding binding) {
+        super();
+        this.binding = binding;
+    }
+}
