78dd5af5e4caa551695f2beaf1a8aa32a9f6e556
RWitak <rafael.witak@gmail.com>
2020-11-07 12:37:59 +0100

    [WIP] Create abstract EditExerciseRow.setPreFilledText() and implement
    Special cases not implemented yet (names, imgURIs)
    
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/editExerciseRows/ImageUriRow.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/editExerciseRows/ImageUriRow.java
@@ -2,12 +2,19 @@ package com.rafaelwitak.gymdatabro.exerciseHandling.editExerciseRows;
 
 import android.widget.EditText;
 
+import com.rafaelwitak.gymdatabro.database.Exercise;
 import com.rafaelwitak.gymdatabro.databinding.ActivityEditExerciseBinding;
 import com.rafaelwitak.gymdatabro.exerciseHandling.EditExerciseRow;
 
 public class ImageUriRow extends EditExerciseRow {
     public ImageUriRow(ActivityEditExerciseBinding binding) {
         super(binding);
+    }
+
+    @Override
+    public void setPreFilledText(Exercise exercise) {
+        //FIXME: correct Database handling of URIS and change this line:
+        this.editText.setText(exercise.imgA_URI);
     }
 
     @Override
