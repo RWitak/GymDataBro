2791bb2a8520f3cf0d3c7b7a9f08a8f3d8e5e66b
RWitak <rafael.witak@gmail.com>
2020-11-07 12:37:59 +0100

    [WIP] Create abstract EditExerciseRow.setPreFilledText() and implement
    Special cases not implemented yet (names, imgURIs)
    
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/editExerciseRows/CuesRow.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/editExerciseRows/CuesRow.java
@@ -2,12 +2,18 @@ package com.rafaelwitak.gymdatabro.exerciseHandling.editExerciseRows;
 
 import android.widget.EditText;
 
+import com.rafaelwitak.gymdatabro.database.Exercise;
 import com.rafaelwitak.gymdatabro.databinding.ActivityEditExerciseBinding;
 import com.rafaelwitak.gymdatabro.exerciseHandling.EditExerciseRow;
 
 public class CuesRow extends EditExerciseRow {
     public CuesRow(ActivityEditExerciseBinding binding) {
         super(binding);
+    }
+
+    @Override
+    public void setPreFilledText(Exercise exercise) {
+        this.editText.setText(exercise.cues);
     }
 
     @Override
--- app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/editExerciseRows/LinksRow.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/editExerciseRows/LinksRow.java
@@ -2,12 +2,18 @@ package com.rafaelwitak.gymdatabro.exerciseHandling.editExerciseRows;
 
 import android.widget.EditText;
 
+import com.rafaelwitak.gymdatabro.database.Exercise;
 import com.rafaelwitak.gymdatabro.databinding.ActivityEditExerciseBinding;
 import com.rafaelwitak.gymdatabro.exerciseHandling.EditExerciseRow;
 
 public class LinksRow extends EditExerciseRow {
     public LinksRow(ActivityEditExerciseBinding binding) {
         super(binding);
+    }
+
+    @Override
+    public void setPreFilledText(Exercise exercise) {
+        this.editText.setText(exercise.links);
     }
 
     @Override
