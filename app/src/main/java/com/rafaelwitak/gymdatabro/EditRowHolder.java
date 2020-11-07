30500c65bef5152630c4edd0c15838b2790f6f8a
RWitak <rafael.witak@gmail.com>
2020-11-07 11:14:11 +0100

    Extract superclass EditRowHolder from EditProgramRowHolder
    Signed-off-by: RWitak <rafael.witak@gmail.com>

---
+++ app/src/main/java/com/rafaelwitak/gymdatabro/EditRowHolder.java
@@ -0,0 +1,13 @@
+package com.rafaelwitak.gymdatabro;
+
+import android.content.Context;
+
+import com.rafaelwitak.gymdatabro.database.GymBroDatabase;
+
+public class EditRowHolder {
+    protected final GymBroDatabase database;
+
+    public EditRowHolder(Context context) {
+        this.database = GymBroDatabase.getDatabase(context);
+    }
+}
