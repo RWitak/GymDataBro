0cc1e34cfbd594e9db0cbba30fec87c5281a558a
RWitak <rafael.witak@gmail.com>
2020-11-08 10:18:56 +0100

    Add TODOs and one empty method
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/activities/WorkoutStepActivity.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/activities/WorkoutStepActivity.java
@@ -184,4 +184,10 @@ public class WorkoutStepActivity extends AppCompatActivity {
         }
         return null;
     }
+
+    public String getCurrentExerciseName() {
+        Integer exerciseId = currentWorkoutStep.exerciseID;
+        //TODO get name by exercise name id (yet to be implemented) or main name if not exists
+        return null;
+    }
 }
