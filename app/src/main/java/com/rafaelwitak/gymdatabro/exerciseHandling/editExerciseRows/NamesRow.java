50fb970ea2f90802725cf3848e2f71ead1d3dd1e
RWitak <rafael.witak@gmail.com>
2020-11-08 19:43:47 +0100

    Pull setPreFilledText() up to EditExerciseRow, change arg to String
    Previous behavior was each EditExerciseRow getting its own data out of a given exercise, is now simply given String.
    
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/editExerciseRows/NamesRow.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/editExerciseRows/NamesRow.java
@@ -2,18 +2,12 @@ package com.rafaelwitak.gymdatabro.exerciseHandling.editExerciseRows;
 
 import android.widget.EditText;
 
-import com.rafaelwitak.gymdatabro.database.Exercise;
 import com.rafaelwitak.gymdatabro.databinding.ActivityEditExerciseBinding;
 import com.rafaelwitak.gymdatabro.exerciseHandling.EditExerciseRow;
 
 public class NamesRow extends EditExerciseRow {
     public NamesRow(ActivityEditExerciseBinding binding) {
         super(binding);
-    }
-
-    @Override
-    public void setPreFilledText(Exercise exercise) {
-        //TODO: implement
     }
 
     @Override
