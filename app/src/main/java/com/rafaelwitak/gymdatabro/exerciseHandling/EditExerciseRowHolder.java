86a26a4ab160e60680fac57c0d583151e5117f4f
RWitak <rafael.witak@gmail.com>
2020-11-09 14:35:52 +0100

    Remove ExerciseName dependencies in EditExerciseRowHolder
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/EditExerciseRowHolder.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/EditExerciseRowHolder.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 import com.rafaelwitak.gymdatabro.EditRowHolder;
 import com.rafaelwitak.gymdatabro.database.Exercise;
-import com.rafaelwitak.gymdatabro.database.ExerciseName;
 import com.rafaelwitak.gymdatabro.databinding.ActivityEditExerciseBinding;
 import com.rafaelwitak.gymdatabro.exerciseHandling.editExerciseRows.CuesRow;
 import com.rafaelwitak.gymdatabro.exerciseHandling.editExerciseRows.EquipmentRow;
@@ -12,7 +11,6 @@ import com.rafaelwitak.gymdatabro.exerciseHandling.editExerciseRows.LinksRow;
 import com.rafaelwitak.gymdatabro.exerciseHandling.editExerciseRows.NamesRow;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class EditExerciseRowHolder extends EditRowHolder {
@@ -47,11 +45,11 @@ public class EditExerciseRowHolder extends EditRowHolder {
     }
 
     public Exercise getUpdatedExercise() {
-        updateExerciseNames(); //TODO: Call externally (would introduce side effects here!)
         return setupExerciseFromRowsInputs(this.exercise);
     }
 
     private Exercise setupExerciseFromRowsInputs(Exercise exercise) {
+        exercise.name = namesRow.getEditTextValueAsString();
         exercise.cues = cuesRow.getEditTextValueAsString();
         exercise.links = linksRow.getEditTextValueAsString();
         exercise.equipment = equipmentRow.getEditTextValueAsString();
@@ -59,38 +57,10 @@ public class EditExerciseRowHolder extends EditRowHolder {
         return exercise;
     }
 
-    private void updateExerciseNames() {
-        //TODO: Update exercise_names table in DB with edited names for exercise
-    }
-
-    private String getExerciseNamesAsString(List<ExerciseName> namesList) {
-        StringBuilder concatenatedExerciseNames = new StringBuilder();
-
-        for (int i = 0; i < namesList.size(); i++) {
-            concatenatedExerciseNames.append(namesList.get(i).name);
-            if (i < namesList.size() - 1) {
-                concatenatedExerciseNames.append("; ");
-            }
-        }
-
-        return concatenatedExerciseNames.toString();
-    }
-
-    private List<ExerciseName> getExerciseNamesList() {
-        // FIXME: Use single name from Exercise's name field
-        List<ExerciseName> exerciseNameList = Collections.singletonList(new ExerciseName());
-        exerciseNameList.get(0).name = "I AM NOT A REAL NAME";
-        return exerciseNameList;
-    }
-
     public void setupRowTexts(Exercise exercise) {
+        namesRow.setPreFilledText(exercise.name);
         cuesRow.setPreFilledText(exercise.cues);
         linksRow.setPreFilledText(exercise.links);
         equipmentRow.setPreFilledText(exercise.equipment);
-        namesRow.setPreFilledText(getTextForExerciseNamePreFill());
-    }
-
-    private String getTextForExerciseNamePreFill() {
-        return getExerciseNamesAsString(getExerciseNamesList());
     }
 }
