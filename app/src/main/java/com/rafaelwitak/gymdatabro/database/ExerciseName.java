7fdebea93ced7b8b7ba989b5c1cf62ddc350ca13
RWitak <rafael.witak@gmail.com>
2020-11-09 14:15:10 +0100

    Remove ExerciseName as Entity, drop table, migrate DB v11->12
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/database/ExerciseName.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/database/ExerciseName.java
@@ -1,32 +1,8 @@
 package com.rafaelwitak.gymdatabro.database;
 
-import androidx.annotation.NonNull;
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.ForeignKey;
-import androidx.room.Index;
-import androidx.room.PrimaryKey;
-
-@Entity(
-        tableName = "exercise_names",
-        foreignKeys = @ForeignKey(
-                entity = Exercise.class,
-                parentColumns = "id",
-                childColumns = "exercise_id"
-        ),
-        indices = {
-                @Index(value = "exercise_id")
-        }
-        )
 public class ExerciseName {
 
-    @PrimaryKey
-    @NonNull
     public String name = "";
-
-    @ColumnInfo(name = "is_main_name")
     public Boolean isMainName;
-
-    @ColumnInfo(name = "exercise_id")
     public int exerciseID;
 }
