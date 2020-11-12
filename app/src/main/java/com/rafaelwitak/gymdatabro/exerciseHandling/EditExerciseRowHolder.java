f871be517c1b1514385fed033c7ee88df96a92bd
RWitak <rafael.witak@gmail.com>
2020-11-12 20:37:09 +0100

    Add currently unused PrRow
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/EditExerciseRowHolder.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/exerciseHandling/EditExerciseRowHolder.java
@@ -13,10 +13,13 @@ import com.rafaelwitak.gymdatabro.exerciseHandling.editExerciseRows.NameRow;
 import java.util.Arrays;
 import java.util.List;
 
+//import com.rafaelwitak.gymdatabro.exerciseHandling.editExerciseRows.PrRow;
+
 public class EditExerciseRowHolder extends EditRowHolder {
     private final Exercise exercise;
 
     private final NameRow nameRow;
+//    private final PrRow prRow;
     private final CuesRow cuesRow;
     private final LinksRow linksRow;
     private final EquipmentRow equipmentRow;
@@ -30,6 +33,7 @@ public class EditExerciseRowHolder extends EditRowHolder {
         this.exercise = exercise;
 
         this.nameRow = new NameRow(binding);
+//        this.prRow = new PrRow(binding);
         this.cuesRow = new CuesRow(binding);
         this.linksRow = new LinksRow(binding);
         this.equipmentRow = new EquipmentRow(binding);
@@ -38,6 +42,7 @@ public class EditExerciseRowHolder extends EditRowHolder {
     public List<EditExerciseRow> getRows() {
         return Arrays.asList(
                 this.nameRow,
+                // this.prRow,
                 this.cuesRow,
                 this.linksRow,
                 this.equipmentRow
@@ -50,6 +55,7 @@ public class EditExerciseRowHolder extends EditRowHolder {
 
     private Exercise setupExerciseFromRowsInputs(Exercise exercise) {
         exercise.name = nameRow.getEditTextValueAsString();
+        // exercise.pr = prRow.getEditTextValueAsFloat();
         exercise.cues = cuesRow.getEditTextValueAsString();
         exercise.links = linksRow.getEditTextValueAsString();
         exercise.equipment = equipmentRow.getEditTextValueAsString();
@@ -59,6 +65,7 @@ public class EditExerciseRowHolder extends EditRowHolder {
 
     public void setupRowTexts(Exercise exercise) {
         nameRow.setPreFilledText(exercise.name);
+        // nameRow.setPreFilledText(exercise.pr);
         cuesRow.setPreFilledText(exercise.cues);
         linksRow.setPreFilledText(exercise.links);
         equipmentRow.setPreFilledText(exercise.equipment);
