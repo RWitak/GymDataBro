e46a03319295a58d67d78fbe707269e4de468493
RWitak <rafael.witak@gmail.com>
2020-11-19 22:22:05 +0100

    Refactor everything in EditWorkoutStepActivity, change button name
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- app/src/main/java/com/rafaelwitak/gymdatabro/activities/EditWorkoutStepActivity.java
+++ app/src/main/java/com/rafaelwitak/gymdatabro/activities/EditWorkoutStepActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -31,12 +32,11 @@ import static com.rafaelwitak.gymdatabro.StringHelper.getNonNullString;
 
 public class EditWorkoutStepActivity extends AppCompatActivity {
 
+    private final HashMap<String, EditText> editTextMap = new HashMap<>();
     private GymBroDatabase database;
     private ActivityEditWorkoutStepBinding binding;
     private WorkoutStep workoutStep;
     private boolean isExistingWorkoutStep;
-    private HashMap<String, EditText> editTexts;
-    private WorkoutStepSaveHandler saveHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,24 +44,17 @@ public class EditWorkoutStepActivity extends AppCompatActivity {
 
         this.database = GymBroDatabase.getDatabase(this);
 
-        Intent intent = getIntent();
-        this.isExistingWorkoutStep = getIsExistingWorkoutStep(intent);
+        this.isExistingWorkoutStep = getIsExistingWorkoutStep(getIntent());
         this.workoutStep = getWorkoutStep(this.isExistingWorkoutStep);
 
         this.binding = ActivityEditWorkoutStepBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        this.editTexts = getEditTexts(this.binding);
+        setEditTextMap();
         setViews();
-
-        this.saveHandler =
-                new WorkoutStepSaveHandler(this,
-                        this.database.workoutStepDAO(),
-                        this.workoutStep,
-                        this.isExistingWorkoutStep);
     }
 
-    private boolean getIsExistingWorkoutStep(Intent intent) {
+    private boolean getIsExistingWorkoutStep(@NonNull Intent intent) {
         return intent.hasExtra("workoutId") && intent.hasExtra("stepNumber");
     }
 
@@ -72,48 +65,45 @@ public class EditWorkoutStepActivity extends AppCompatActivity {
         return new WorkoutStep();
     }
 
-    private WorkoutStep getWorkoutStepFromIntent(Intent intent) {
+    private WorkoutStep getWorkoutStepFromIntent(@NonNull Intent intent) {
         int workoutId = intent.getIntExtra("workoutId", -1);
         int stepNumber = intent.getIntExtra("stepNumber", -1);
         return database.workoutStepDAO().getWorkoutStepSynchronously(workoutId, stepNumber);
     }
 
-    private HashMap<String, EditText> getEditTexts(
-            ActivityEditWorkoutStepBinding binding) {
-        HashMap<String, EditText> map = new HashMap<>();
 
-        map.put("Name", binding.editWorkoutStepNameEdit);
-        map.put("WorkoutID", binding.editWorkoutStepWorkoutIdEdit);
-//        map.put("Number", binding.editWorkoutStepNumberEdit);
-        map.put("ExerciseID", binding.editWorkoutStepExerciseIdEdit);
-        map.put("Reps", binding.editWorkoutStepRepsEdit);
-        map.put("Weight", binding.editWorkoutStepWeightEdit);
-        map.put("RPE", binding.editWorkoutStepRpeEdit);
-        map.put("Duration", binding.editWorkoutStepDurationEdit);
-        map.put("Rest", binding.editWorkoutStepRestEdit);
-        map.put("Details", binding.editWorkoutStepDetailsEdit);
-        map.put("Notes", binding.editWorkoutStepNotesEdit);
-
-        return map;
+    private void setEditTextMap() {
+        editTextMap.put("Name", binding.editWorkoutStepNameEdit);
+        editTextMap.put("WorkoutID", binding.editWorkoutStepWorkoutIdEdit);
+//        editTextMap.put("Number", binding.editWorkoutStepNumberEdit); // obsolete atm
+        editTextMap.put("ExerciseID", binding.editWorkoutStepExerciseIdEdit);
+        editTextMap.put("Reps", binding.editWorkoutStepRepsEdit);
+        editTextMap.put("Weight", binding.editWorkoutStepWeightEdit);
+        editTextMap.put("RPE", binding.editWorkoutStepRpeEdit);
+        editTextMap.put("Duration", binding.editWorkoutStepDurationEdit);
+        editTextMap.put("Rest", binding.editWorkoutStepRestEdit);
+        editTextMap.put("Details", binding.editWorkoutStepDetailsEdit);
+        editTextMap.put("Notes", binding.editWorkoutStepNotesEdit);
     }
 
 
     private void setViews() {
         setupToolbar(binding.editWorkoutStepToolbar.getRoot());
         setupEditTexts();
-        setupEditButton();
+        setupSaveButton();
 
         new SpinnerManager().setup();
     }
 
-    private void setupToolbar(Toolbar toolbar) {
+    private void setupToolbar(@NonNull Toolbar toolbar) {
         toolbar.setTitle(isExistingWorkoutStep ? "Edit WorkoutStep" : "Create WorkoutStep");
     }
+
 
     private void setupEditTexts() {
         setTextsSafely("Name", workoutStep.name);
         setTextsSafely("WorkoutID", workoutStep.workoutID);
-//        setTextsSafely("Number", workoutStep.number);
+//        setTextsSafely("Number", workoutStep.number);  // obsolete atm
         setTextsSafely("ExerciseID", workoutStep.exerciseID);
         setTextsSafely("Reps", workoutStep.reps);
         setTextsSafely("Weight", workoutStep.weight);
@@ -126,46 +116,100 @@ public class EditWorkoutStepActivity extends AppCompatActivity {
 
     public void setTextsSafely(String key, String text) {
         String sanitizedText = getNonNullString(text);
-        Objects.requireNonNull(
-                this.editTexts.get(key))
-                .setText(sanitizedText);
+        getEditTextSafely(key).setText(sanitizedText);
     }
 
     public void setTextsSafely(String key, Number value) {
         String sanitizedText = getNonNullString(value);
-        Objects.requireNonNull(
-                this.editTexts.get(key))
-                .setText(sanitizedText);
-    }
-
-    private void setupEditButton() {
-        Button button = binding.editWorkoutStepButtonEdit;
-        button.setOnClickListener(v -> tryToSaveAndExit());
+        getEditTextSafely(key).setText(sanitizedText);
     }
 
 
-    private void tryToSaveAndExit() {
-        WorkoutStep updatedWorkoutStep =
-                updateWorkoutStepFromEditTexts(this.workoutStep, this.editTexts);
+    private void setupSaveButton() {
+        Button button = binding.editWorkoutStepButtonSave;
+        button.setOnClickListener(v -> tryToSaveAndFinish());
+    }
 
-        int sanityStatus = WorkoutStepSanityChecker.getStatus(
-                updatedWorkoutStep,
-                this.database.workoutStepDAO(),
-                this.database.exerciseDAO(),
-                this.isExistingWorkoutStep);
+    private void tryToSaveAndFinish() {
+        updateWorkoutStepFromEditTexts();
+
+        int sanityStatus = getSanityStatus();
 
         if (sanityStatus == WorkoutStepSanityChecker.Status.SAVABLE) {
-            this.saveHandler.saveAndFinish();
+            saveAndFinish();
+        } else {
+            handleSanityStatusErrors(sanityStatus);
         }
+    }
 
-        else if (WorkoutStepSanityChecker.Status.isBadName(sanityStatus)) {
-            this.editTexts.get("Name").setError("Please choose a meaningful name or leave empty.");
+    private void updateWorkoutStepFromEditTexts() {
+
+        workoutStep.workoutID = getNonNullIntFromEditText("WorkoutID");
+        workoutStep.exerciseID = getNonNullIntFromEditText("ExerciseID");
+
+        workoutStep.name = getTextAsTrimmedStringOrNull(getEditTextSafely("Name"));
+// workoutStep.number = getTextAsNullableInteger(getEditTextSafely("Number")); // obsolete atm
+        workoutStep.reps = getTextAsNullableInteger(getEditTextSafely("Reps"));
+        workoutStep.weight = getTextAsNullableFloat(getEditTextSafely("Weight"));
+        workoutStep.rpe = getTextAsNullableFloat(getEditTextSafely("RPE"));
+        workoutStep.durationSeconds = getTextAsNullableInteger(getEditTextSafely("Duration"));
+        workoutStep.restSeconds = getTextAsNullableInteger(getEditTextSafely("Rest"));
+        workoutStep.details = getTextAsTrimmedStringOrNull(getEditTextSafely("Details"));
+        workoutStep.notes = getTextAsTrimmedStringOrNull(getEditTextSafely("Notes"));
+
+        if (!isExistingWorkoutStep) {
+            workoutStep.number = getNextWorkoutStepNumber();
+        }
+    }
+
+    private void saveAndFinish() {
+        getSaveHandler().saveAndFinish();
+    }
+
+    private int getSanityStatus() {
+        return WorkoutStepSanityChecker.getStatus(
+                    workoutStep,
+                    database.workoutStepDAO(),
+                    database.exerciseDAO(),
+                    isExistingWorkoutStep);
+    }
+
+    @NonNull
+    private WorkoutStepSaveHandler getSaveHandler() {
+        return new WorkoutStepSaveHandler(
+                this,
+                this.database.workoutStepDAO(),
+                this.workoutStep,
+                this.isExistingWorkoutStep);
+    }
+
+    private int getNonNullIntFromEditText(String editTextKey) {
+        return Integer.parseInt(getEditTextSafely(editTextKey).getText().toString());
+    }
+
+    @NonNull
+    private EditText getEditTextSafely(String key) {
+        return Objects.requireNonNull(editTextMap.get(key));
+    }
+
+    private int getNextWorkoutStepNumber() {
+        return database.workoutStepDAO()
+                .getAllStepsForWorkoutSynchronously(workoutStep.workoutID)
+                .size();
+    }
+
+    private void handleSanityStatusErrors(int sanityStatus) {
+        if (WorkoutStepSanityChecker.Status.isBadName(sanityStatus)) {
+            getEditTextSafely("Name")
+                    .setError("Please choose a meaningful name or leave empty.");
         }
         else if (WorkoutStepSanityChecker.Status.isBadExerciseId(sanityStatus)) {
-            this.editTexts.get("ExerciseID").setError("Please choose an existing ExerciseID.");
+            getEditTextSafely("ExerciseID")
+                    .setError("Please choose an existing ExerciseID.");
         }
         else if (WorkoutStepSanityChecker.Status.isBadRpe(sanityStatus)) {
-            this.editTexts.get("RPE").setError("RPE must be a positive number up to 10.");
+            getEditTextSafely("RPE")
+                    .setError("RPE must be a positive number up to 10.");
         }
         else if (WorkoutStepSanityChecker.Status.isBadNumber(sanityStatus)) {
             throw new RuntimeException("SanityCheck Error (WorkoutStep): " +
@@ -178,84 +222,52 @@ public class EditWorkoutStepActivity extends AppCompatActivity {
         }
     }
 
-    private WorkoutStep updateWorkoutStepFromEditTexts(
-            WorkoutStep workoutStep,
-            HashMap<String, EditText> editTexts) {
-
-        workoutStep.name = getTextAsTrimmedStringOrNull(editTexts.get("Name"));
-        workoutStep.workoutID = getTextAsNullableInteger(editTexts.get("WorkoutID"));
-//        workoutStep.number = getTextAsNullableInteger(editTexts.get("Number")); // obsolete atm
-        workoutStep.exerciseID = getTextAsNullableInteger(editTexts.get("ExerciseID"));
-        workoutStep.reps = getTextAsNullableInteger(editTexts.get("Reps"));
-        workoutStep.weight = getTextAsNullableFloat(editTexts.get("Weight"));
-        workoutStep.rpe = getTextAsNullableFloat(editTexts.get("RPE"));
-        workoutStep.durationSeconds = getTextAsNullableInteger(editTexts.get("Duration"));
-        workoutStep.restSeconds = getTextAsNullableInteger(editTexts.get("Rest"));
-        workoutStep.details = getTextAsTrimmedStringOrNull(editTexts.get("Details"));
-        workoutStep.notes = getTextAsTrimmedStringOrNull(editTexts.get("Notes"));
-
-        if (!isExistingWorkoutStep) {
-            workoutStep.number = getNextWorkoutStepNumber(workoutStep);
-        }
-        return workoutStep;
-    }
-
-    private int getNextWorkoutStepNumber(WorkoutStep workoutStep) {
-        return database.workoutStepDAO().getAllStepsForWorkoutSynchronously(workoutStep.workoutID).size();
-    }
-
 
     private class SpinnerManager {
 
         public void setup() {
-            setupAllSpinners(getSpinners(), database);
+
+            Spinner workoutIdSpinner = binding.editWorkoutStepWorkoutIdSpinner;
+            Spinner exerciseIdSpinner = binding.editWorkoutStepExerciseIdSpinner;
+
+            setupSpinner(workoutIdSpinner, getWorkoutNamesArray(), getWorkoutListener());
+            setupSpinner(exerciseIdSpinner, getExerciseNamesArray(), getExerciseListener());
         }
 
-        private void setupAllSpinners(HashMap<String, Spinner> spinners, GymBroDatabase database) {
+        private void setupSpinner(@NonNull Spinner spinner,
+                                  ArrayList<CharSequence> arrayList,
+                                  AdapterView.OnItemSelectedListener listener) {
 
-            Spinner exerciseIdSpinner = spinners.get("ExerciseIdSpinner");
-            Spinner workoutIdSpinner = spinners.get("WorkoutIdSpinner");
-
-            setupExerciseSpinner(database, exerciseIdSpinner, getExerciseNamesArray(database));
-            setupWorkoutIdSpinner(database, workoutIdSpinner, getWorkoutNamesArray(database));
-        }
-
-        private void setupWorkoutIdSpinner(GymBroDatabase database,
-                                           Spinner spinner,
-                                           ArrayList<String> workoutNames) {
-
-            ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(
-                    getBaseContext(),
-                    android.R.layout.simple_spinner_item,
-                    new ArrayList<>());
+            ArrayAdapter<CharSequence> spinnerAdapter =
+                    new ArrayAdapter<>(getBaseContext(),
+                            android.R.layout.simple_spinner_item,
+                            arrayList);
             spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
             spinner.setAdapter(spinnerAdapter);
-            spinner.setOnItemSelectedListener(getWorkoutListener(database));
-
-            // As suggested by https://stackoverflow.com/a/61875814/12121416
-            spinnerAdapter.addAll(workoutNames);
-            spinnerAdapter.notifyDataSetChanged();
+            spinner.setOnItemSelectedListener(listener);
         }
 
-        private AdapterView.OnItemSelectedListener getWorkoutListener(GymBroDatabase database) {
+        @NonNull
+        private AdapterView.OnItemSelectedListener getWorkoutListener() {
             return new AdapterView.OnItemSelectedListener() {
                 @Override
-                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                public void onItemSelected(@NonNull AdapterView<?> adapterView, View view, int i, long l) {
                     Workout workout = database.workoutDAO().getWorkoutByName(
                             (String) adapterView.getItemAtPosition(i));
                     binding.editWorkoutStepWorkoutIdEdit.setText(String.valueOf(workout.id));
                 }
 
                 @Override
-                public void onNothingSelected(AdapterView<?> adapterView) {
+                public void onNothingSelected(@NonNull AdapterView<?> adapterView) {
                     adapterView.setSelection(0);
                 }
             };
         }
 
-        private ArrayList<String> getWorkoutNamesArray(GymBroDatabase database) {
-            ArrayList<String> workoutNames = new ArrayList<>();
+        @NonNull
+        private ArrayList<CharSequence> getWorkoutNamesArray() {
+            ArrayList<CharSequence> workoutNames = new ArrayList<>();
 
             for (Workout workout : database.workoutDAO().getAllWorkouts()) {
                 workoutNames.add(workout.name);
@@ -264,55 +276,31 @@ public class EditWorkoutStepActivity extends AppCompatActivity {
         }
 
 
-        private void setupExerciseSpinner(GymBroDatabase database,
-                                          Spinner exerciseIdSpinner,
-                                          ArrayList<CharSequence> exerciseNames) {
-
-            ArrayAdapter<CharSequence> adapter =
-                    new ArrayAdapter<>(getBaseContext(),
-                            android.R.layout.simple_spinner_item,
-                            exerciseNames);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-            exerciseIdSpinner.setAdapter(adapter);
-            exerciseIdSpinner.setOnItemSelectedListener(getExerciseListener(database));
-        }
-
-        private AdapterView.OnItemSelectedListener getExerciseListener(GymBroDatabase database) {
+        @NonNull
+        private AdapterView.OnItemSelectedListener getExerciseListener() {
             return new AdapterView.OnItemSelectedListener() {
                 @Override
-                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                public void onItemSelected(@NonNull AdapterView<?> adapterView, View view, int i, long l) {
                     Exercise exercise = database.exerciseDAO().getExercisesByName(
                             adapterView.getItemAtPosition(i).toString()).get(0);
                     binding.editWorkoutStepExerciseIdEdit.setText(String.valueOf(exercise.id));
                 }
 
                 @Override
-                public void onNothingSelected(AdapterView<?> adapterView) {
+                public void onNothingSelected(@NonNull AdapterView<?> adapterView) {
                     adapterView.setSelection(0);
                 }
             };
         }
 
-        private ArrayList<CharSequence> getExerciseNamesArray(GymBroDatabase database) {
-            ArrayList<CharSequence> names = new ArrayList<>();
+        @NonNull
+        private ArrayList<CharSequence> getExerciseNamesArray() {
+            ArrayList<CharSequence> exerciseNames = new ArrayList<>();
+
             for (Exercise exercise : database.exerciseDAO().getAllExercises()) {
-                names.add(exercise.name);
+                exerciseNames.add(exercise.name);
             }
-            return names;
-        }
-
-        private HashMap<String, Spinner> getSpinners() {
-            HashMap<String, Spinner> spinnerHashMap = new HashMap<>();
-
-            spinnerHashMap.put(
-                    "WorkoutIdSpinner",
-                    binding.editWorkoutStepWorkoutIdSpinner);
-            spinnerHashMap.put(
-                    "ExerciseIdSpinner",
-                    binding.editWorkoutStepExerciseIdSpinner);
-
-            return spinnerHashMap;
+            return exerciseNames;
         }
     }
 }
