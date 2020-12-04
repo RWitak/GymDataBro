78a590aaed9c0ad79cfcbbdd9aa09cd5e979d6b9
RWitak <rafael.witak@gmail.com>
2020-12-04 20:12:29 +0100

    Add WorkoutInstanceDAO

---
+++ app/src/main/java/com/rafaelwitak/gymdatabro/database/WorkoutInstanceDAO.java
@@ -0,0 +1,19 @@
+package com.rafaelwitak.gymdatabro.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface WorkoutInstanceDAO {
+    @Query("SELECT name FROM workout_instances WHERE id=:id")
+    String getNameByInstanceId(int id);
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    long insertWorkoutInstance(WorkoutInstance workoutInstance);
+
+    @Update(onConflict = OnConflictStrategy.ABORT)
+    void updateWorkoutInstance(WorkoutInstance workoutInstance);
+}
