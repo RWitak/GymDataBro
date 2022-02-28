09ca955d76cf20442d589b9ee1a695f8a0b2f8bb
RWitak <rafael.witak@gmail.com>
2022-02-28 10:58:57 +0100

    [#13][Minor] Update DB data.
    Signed-off-by: RWitak <rafael.witak@gmail.com>

--- py_utils/workout_step_sql_creator.py
+++ py_utils/workout_step_sql_creator.py
@@ -8,25 +8,27 @@ import datetime
 import os
 from textwrap import dedent
 
-EXERCISES = (2, 12, 25, 18, 28, 29, 20,
-             1, 11, 16, 17, 19, 15, 39,
-             3, 22, 31, 32, 29, 14, 33,
-             23, 24, 13, 34, 27, 26, 35)
+# Don't forget to first create all exercises, workouts, and workout_instances!
 
-REPS = (6, 10, 12, 12, 12, 6, 12,
-        5, 10, 10, 12, 12, 10, 12,
-        8, 10, 15, 15, 15, 12, 20,
-        8, 8, 10, 12, 15, 15, 8)
+EXERCISES = (3, 40, 41, 42, 28, 14, 43,
+             1, 44, 45, 46, 13, 26, 47,
+             2, 22, 12, 36, 14, 49, 29,
+             16, 50, 23, 37, 52, 53, 54)
 
-RPES = (7, 7, 8, 9, 9, 7, 7,
-        7, 8, 7, 8, 8, 8, 8,
-        7, 8, 8, 8, 9, 8, 8,
-        8, 8, 7, 7, 8, 8, 9)
+REPS = (5, 12, 10, 12, 15, 8, 12,
+        8, 8, 12, 12, 6, 15, 12,
+        8, 8, 12, 8, 6, 6, 20,
+        6, 6, 8, 8, 15, 15, 15)
+
+RPES = (8, 8, 9, 8, 9, 8, 8,
+        8, 8, 7, 8, 8, 9, 9,
+        8, 8, 8, 9, 9, 8, 9,
+        8, 8, 8, 9, 8, 8, 9)
 
 RESTS = (210, 150, 150, 90, 90, 90, 90,
-         210, 150, 210, 150, 90, 90, 90,
-         210, 150, 90, 90, 90, 90, 90,
-         150, 150, 150, 150, 90, 90, 90)
+         210, 150, 150, 150, 90, 90, 90,
+         210, 150, 150, 90, 90, 90, 90,
+         210, 210, 150, 150, 90, 90, 90)
 
 
 def _make_query() -> str:
@@ -36,7 +38,7 @@ def _make_query() -> str:
     rpe = iter(RPES)
     rest = iter(RESTS)
 
-    for workout in range(19, 23):
+    for workout in range(23, 27):
         for number in range(7):
             curr_exercise = next(exercise)
             curr_rep = next(rep)
@@ -45,15 +47,15 @@ def _make_query() -> str:
 
             for i in range(0, 3):
                 _set = number * 3 + i
-
-                if workout == 21 and _set in (18, 19, 20):
-                    query += dedent(f'''
-                    INSERT INTO "main"."workout_steps"
-                    ("workout_id", "number", "exercise_id",
-                    "duration_seconds", "weight", "rpe", "rest_seconds")
-                    VALUES ({workout}, {_set}, {curr_exercise},
-                    {curr_rep}, 0, {curr_rpe}, {curr_rest});''')
-                    continue
+                #
+                # if workout == 21 and _set in (18, 19, 20):
+                #     query += dedent(f'''
+                #     INSERT INTO "main"."workout_steps"
+                #     ("workout_id", "number", "exercise_id",
+                #     "duration_seconds", "weight", "rpe", "rest_seconds")
+                #     VALUES ({workout}, {_set}, {curr_exercise},
+                #     {curr_rep}, 0, {curr_rpe}, {curr_rest});''')
+                #     continue
 
                 query += dedent(f'''
                 INSERT INTO "main"."workout_steps"
